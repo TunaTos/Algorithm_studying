621b2c603f152894aca6a67b9473ecda95083747
TunaTos <131116197+TunaTos@users.noreply.github.com>
2025-01-04 19:15:54 +0900

    01-04

---
+++ baekjoon/1759.cc
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename container>
+void showvector(const container& c)
+{
+	for(const auto& elem: c)
+		cout << elem;
+}
+
+class Solver
+{
+private:
+	int N, M;
+	vector<int> visited;
+	vector<char> chars;
+public:
+	Solver(const int& n_input, const int& m_input) : 
+		N(n_input), M(m_input), visited(M,false), chars(M) 
+	{
+		std::fill(visited.begin(), visited.begin() + N, 1);
+		for (int i = 0; i < M; i++)
+			cin >> chars[i];
+		sort(chars.begin(), chars.end());
+	}
+	
+	void combination()
+	{
+		do
+		{
+			int vowel_num{0}, consonent_num{0};
+			vector<char> vec;
+			for (int i = 0; i < M; i++)
+			{
+				if (visited[i] == 1)
+				{
+					vec.push_back(chars[i]);
+					if (chars[i] == 'a' || chars[i] == 'e' || chars[i] == 'i' || chars[i] =='o' || chars[i] =='u')
+						vowel_num++;
+					else
+						consonent_num++;
+				}
+			}
+			if (vowel_num != 0 && consonent_num >= 2)
+			{
+				showvector(vec);
+				std::cout << '\n';
+			}
+		} while (std::prev_permutation(visited.begin(), visited.end()));
+	}
+
+};
+
+void solve()
+{
+	int N, M;
+	cin >> N >> M;
+	Solver S(N, M);
+	S.combination();
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	solve();
+
+	return 0;
+}
